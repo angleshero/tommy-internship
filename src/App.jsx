0ddf68ddf91517823669a2dae1466a53c24562de
tommy <example@email.com>
2026-04-29 14:17:32 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <>
       <Nav />
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
